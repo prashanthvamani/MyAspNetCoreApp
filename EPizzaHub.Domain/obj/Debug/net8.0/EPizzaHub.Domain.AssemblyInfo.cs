282a73c970b2ee12de0504c79a18038854f12d2d
prashanth <prashanthvamani39@gmail.com>
2025-03-15 23:26:20 +0530

    Saving my local changes

--- EPizzaHub.Domain/obj/Debug/net8.0/EPizzaHub.Domain.AssemblyInfo.cs
+++ EPizzaHub.Domain/obj/Debug/net8.0/EPizzaHub.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EPizzaHub.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ab3052b1951bb79bf33ecd3ad21e88d95439933")]
 [assembly: System.Reflection.AssemblyProductAttribute("EPizzaHub.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EPizzaHub.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
