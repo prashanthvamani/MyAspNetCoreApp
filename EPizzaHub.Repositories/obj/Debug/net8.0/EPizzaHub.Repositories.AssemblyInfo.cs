a1c47c2f92bf5f29c3cec71911b269baed903180
prashanth <prashanthvamani39@gmail.com>
2025-03-19 00:30:19 +0530

    cart Icartapi added

--- EPizzaHub.Repositories/obj/Debug/net8.0/EPizzaHub.Repositories.AssemblyInfo.cs
+++ EPizzaHub.Repositories/obj/Debug/net8.0/EPizzaHub.Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EPizzaHub.Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+282a73c970b2ee12de0504c79a18038854f12d2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb743fe29c933cb306f3e647c2b2cad06f138746")]
 [assembly: System.Reflection.AssemblyProductAttribute("EPizzaHub.Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EPizzaHub.Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
